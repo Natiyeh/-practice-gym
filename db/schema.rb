969cdecc4d5f2dff4b69566dbd27b09506dae92a
Nasser Atiyeh <nasseratiyeh@gmail.com>
2020-04-05 17:37:59 -0500

    added merch model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_222516) do
+ActiveRecord::Schema.define(version: 2020_04_05_223727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_04_05_222516) do
   end
 
   create_table "memberships", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchandises", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "price"
