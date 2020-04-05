7ed7673071cba8d5a5263c1759002bd59b72fc78
Nasser Atiyeh <nasseratiyeh@gmail.com>
2020-04-05 17:36:14 -0500

    added courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,46 @@
+class CoursesController < ApplicationController
+  before_action :set_course, only: %i[show edit update destroy]
+
+  def index
+    @courses = Course.all
+  end
+
+  def show
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.save
+
+    redirect_to course_path(@course)
+  end
+
+  def edit
+  end
+
+  def update
+    @course.update(course_params)
+
+    redirect_to course_path(@course)
+  end
+
+  def destroy
+    @course.destroy
+
+    redirect_to courses_path
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :description)
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get    'pages/about'
 
   resources :memberships
+  resources :courses
 end
