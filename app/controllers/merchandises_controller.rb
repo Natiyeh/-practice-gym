6decc7892091f237495489e49e71a6010567ca03
Nasser Atiyeh <nasseratiyeh@gmail.com>
2020-04-06 17:51:07 -0500

    added merchandise contollers

---
+++ app/controllers/merchandises_controller.rb
@@ -0,0 +1,46 @@
+class MerchandisesController < ApplicationController
+  before_action :set_merchandise, only: %i[show edit update destroy]
+
+  def index
+    @merchandises = Merchandise.all
+  end
+
+  def show
+  end
+
+  def new
+    @merchandise = Merchandise.new
+  end
+
+  def create
+    @merchandise = Merchandise.new(merchandise_params)
+    @merchandise.save
+
+    redirect_to merchandise_path(@merchandise)
+  end
+
+  def edit
+  end
+
+  def update
+    @merchandise.update(merchandise_params)
+
+    redirect_to merchandise_path(@merchandise)
+  end
+
+  def destroy
+    @merchandise.destroy
+
+    redirect_to merchandises_path
+  end
+
+  private
+
+  def merchandise_params
+    params.require(:merchandise).permit(:name, :description, :price)
+  end
+
+  def set_merchandise
+    @merchandise = Merchandise.find(params[:id])
+  end
+end
