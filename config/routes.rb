216b8660a8bbfb8943e03bca6223cfc7307d5434
Nasser Atiyeh <nasseratiyeh@gmail.com>
2020-04-07 12:24:53 -0500

    added views for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get    'pages/contact'
   get    'pages/about'
 
+  resources :users, only: [:show, :update, :edit]
+
   resources :memberships
   resources :courses
   resources :merchandises
